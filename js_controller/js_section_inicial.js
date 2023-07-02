ddb24ac8ff7a7801a3b00e10bce2309da6546ec3
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-07-02 09:21:16 -0300

    arrumando link da nav

--- js_controller/js_section_inicial.js
+++ js_controller/js_section_inicial.js
@@ -1,30 +1,29 @@
 var frasesDiv = document.getElementById('frases_inicial');
-        var imgDiv = document.getElementById('img_inicial');
-        var isFraseVisible = true;
-        var isImgVisible = true;
+    var imgDiv = document.getElementById('img_inicial');
+    var isFraseVisible = true;
+    var isImgVisible = true;
 
-        window.addEventListener('scroll', function () {
-            var scrollPosition = window.scrollY;
+    window.addEventListener('scroll', function () {
+        var scrollPosition = window.scrollY;
 
-            if (scrollPosition >= 200 && isFraseVisible) {
-                frasesDiv.classList.remove('fade-in-left');
-                frasesDiv.classList.add('fade-out-left');
-                isFraseVisible = false;
-            } else if (scrollPosition < 200 && !isFraseVisible) {
-                frasesDiv.classList.remove('fade-out-left');
-                frasesDiv.classList.add('fade-in-left');
-                isFraseVisible = true;
-            }
+        if (scrollPosition >= 200 && isFraseVisible) {
+            frasesDiv.classList.remove('fade-in-left');
+            frasesDiv.classList.add('fade-out-left');
+            isFraseVisible = false;
+        } else if (scrollPosition < 200 && !isFraseVisible) {
+            frasesDiv.classList.remove('fade-out-left');
+            frasesDiv.classList.add('fade-in-left');
+            isFraseVisible = true;
+        }
 
-            if (scrollPosition >= 200 && isImgVisible) {
-                imgDiv.classList.remove('fade-in-right');
-                imgDiv.classList.add('fade-out-right');
-                isImgVisible = false;
-            } else if (scrollPosition < 200 && !isImgVisible) {
-                imgDiv.classList.remove('fade-out-right');
-                imgDiv.classList.add('fade-in-right');
-                isImgVisible = true;
-            }
+        if (scrollPosition >= 200 && isImgVisible) {
+            imgDiv.classList.remove('fade-in-right');
+            imgDiv.classList.add('fade-out-right');
+            isImgVisible = false;
+        } else if (scrollPosition < 200 && !isImgVisible) {
+            imgDiv.classList.remove('fade-out-right');
+            imgDiv.classList.add('fade-in-right');
+            isImgVisible = true;
+        }
 
-            
-        });
+    });
