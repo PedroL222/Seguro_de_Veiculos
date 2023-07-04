5b6611b0919ee891bcaa0f746145986885ee897c
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-07-04 09:45:23 -0300

    Atualizando scroll e navBar em celulares

--- js_controller/scroll.js
+++ js_controller/scroll.js
@@ -1,9 +1,17 @@
-$('nav a').click(function(e){
+$('nav a').click(function(e) {
     e.preventDefault();
-    let menuHeight = $('nav').innerHeight();
-    let id = $(this).attr('href'),
-    targetOffset = $(id).offset();
-    $('html, body').animate({
+    let id = $(this).attr('href');
+    let targetOffset = $(id).offset();
+  
+    if ($(window).width() <= 992) {
+      let menuHeight = 60.872;
+  
+      $('html, body').animate({
         scrollTop: targetOffset.top - menuHeight
-    }, 500);
-})
+      }, 500);
+    } else {
+      $('html, body').animate({
+        scrollTop: targetOffset.top - $('#navBar').innerHeight()
+      }, 300);
+    }
+  });
